7c7cee0914ea27dc4791572c33724fea2def9c6d
Stephen <stephen.lyssy@gmail.com>
2022-06-02 21:15:48 -0500

    Added code to change the background color of the winning cells.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
-const board = [
+//# Creating global variables that will be updated by the functions below.
+let board = [
   ['', '', ''],
   ['', '', ''],
   ['', '', ''],
@@ -6,6 +7,17 @@ const board = [
 
 let row;
 let column;
+
+//# Selecting diagonal cells.
+const diagonalCellsLeft = document.getElementsByClassName('diagonal-cell-left');
+const diagonalCellsRight = document.getElementsByClassName(
+  'diagonal-cell-right'
+);
+
+//# Selecting vertical columns
+const verticalLeft = document.getElementsByClassName('vertical-cell-left');
+const verticalMiddle = document.getElementsByClassName('vertical-cell-middle');
+const verticalRight = document.getElementsByClassName('vertical-cell-right');
 
 //       ***********************
 //            INSTRUCTIONS
@@ -21,18 +33,18 @@ let currentMarker = 'X';
 // this "handleClick" function is called when a box is clicked. Here, "element" will hold the same value as "this" does in the HTML.
 // "this" is a special word in JS but "element" could have been "thing" or "el" or whatever we wanted it to be as long as we use it again in the "console.log" statement
 const handleClick = (element) => {
-  // this uses the "log" method on the "console" to log out the element's id so we can see it with our human eyes
+  //? this uses the "log" method on the "console" to log out the element's id so we can see it with our human eyes
   console.log(`The element you clicked on has an id:  ${element.id}`);
   row = parseInt(element.id.charAt(0));
   column = parseInt(element.id.charAt(2));
-  // this next line prevents an X being changed to an O or an O being changed to an X by...
-  //  checking to see if the square clicked has anything in it, if not continue
+  //? this next line prevents an X being changed to an O or an O being changed to an X by...
+  //?  checking to see if the square clicked has anything in it, if not continue
   if (!document.getElementById(element.id).innerHTML) {
     addMarker(element.id);
   }
 };
 
-// this function places the "currentMarker" inside the HTML element that was clicked and calls the "changeMarker" function.
+//* This function places the "currentMarker" inside the HTML element that was clicked and calls the "changeMarker" function.
 const addMarker = (id) => {
   board[row][column] = currentMarker;
 
@@ -45,15 +57,11 @@ const addMarker = (id) => {
   // @TODO-2: Build a line of code that will set the innerHTML property of the element that was clicked to the "currentMarker"
 
   // @TODO-2.5: MIX & MATCH, You will need the following pieces of code to build that line:
-  // = currentMarker
-  // .getElementById(id)
-  // document
-  // .innerHTML
   document.getElementById(id).innerHTML = currentMarker;
   checkForWin();
 };
 
-// This "changeMarker" function changes "X" to "O" in the "currentMarker" variable or "O" to "X"
+//* This "changeMarker" function changes "X" to "O" in the "currentMarker" variable or "O" to "X"
 const changeMarker = () => {
   if (currentMarker === 'X') {
     currentMarker = 'O';
@@ -62,52 +70,46 @@ const changeMarker = () => {
   }
 };
 
-// This "resetBoard" function is called when the user clicks on the "Restart" button.
+//* This "resetBoard" function is called when the user clicks on the "Restart" button.
 const resetBoard = () => {
   // @TODO-3: To make your "Restart" button work you'll need to build a line of code here that:
-  // collects all of the "td" elements into an HTML Collection: https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp
+  // ?collects all of the "td" elements into an HTML Collection: https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp
 
   // @TODO-3.5: MIX & MATCH, You will need the following pieces of code to build
-  // that line:
-  // squares
-  // .getElementsByTagName("TD")
-  // =
-  // document
-  // const
   const squares = document.getElementsByTagName('td');
-  // loops over the HTML Collection of TDs and clears out the Xs and Os
+  //? loops over the HTML Collection of TDs and clears out the Xs and Os
   for (i = 0; i < squares.length; i++) {
-    // will log out the id of each square as it loops over them.
+    //? will log out the id of each square as it loops over them.
     console.log(squares[i].id);
 
-    // sets the innerHTML to null to replace the "X" or "O"
+    //? sets the innerHTML to null to replace the "X" or "O"
     squares[i].innerHTML = null;
   }
+  //
+  //# Added this to refresh the page so that the background colors are removed.
+  location.reload();
 };
 
-const checkForWin = () => {
-  if (horizontalWin() || verticalWin() || diagonalWin()) {
-    window.alert(`Player ${currentMarker} won!`);
-  } else {
-    changeMarker();
-  }
-};
-
+//* Below is the logic that will determine verticle, horizontal and diagnal
+//* wins.
 const horizontalWin = () => {
   if (
     (board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X') ||
     (board[0][0] == 'O' && board[0][1] == 'O' && board[0][2] == 'O')
   ) {
+    document.querySelector('#top-row').classList.add('winner');
     return true;
   } else if (
     (board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X') ||
     (board[1][0] == 'O' && board[1][1] == 'O' && board[1][2] == 'O')
   ) {
+    document.querySelector('#middle-row').classList.add('winner');
     return true;
   } else if (
     (board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] == 'X') ||
     (board[2][0] == 'O' && board[2][1] == 'O' && board[2][2] == 'O')
   ) {
+    document.querySelector('#bottom-row').classList.add('winner');
     return true;
   }
 };
@@ -117,16 +119,25 @@ const verticalWin = () => {
     (board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X') ||
     (board[0][0] == 'O' && board[1][0] == 'O' && board[2][0] == 'O')
   ) {
+    Array.from(verticalLeft).forEach((cell) => {
+      cell.classList.add('winner');
+    });
     return true;
   } else if (
     (board[0][1] == 'X' && board[1][1] == 'X' && board[2][1] == 'X') ||
     (board[0][1] == 'O' && board[1][1] == 'O' && board[2][1] == 'O')
   ) {
+    Array.from(verticalMiddle).forEach((cell) => {
+      cell.classList.add('winner');
+    });
     return true;
   } else if (
     (board[0][2] == 'X' && board[1][2] == 'X' && board[2][2] == 'X') ||
     (board[0][2] == 'O' && board[1][2] == 'O' && board[2][2] == 'O')
   ) {
+    Array.from(verticalRight).forEach((cell) => {
+      cell.classList.add('winner');
+    });
     return true;
   }
 };
@@ -136,11 +147,26 @@ const diagonalWin = () => {
     (board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X') ||
     (board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O')
   ) {
+    Array.from(diagonalCellsLeft).forEach((cell) => {
+      cell.classList.add('winner');
+    });
     return true;
   } else if (
     (board[0][2] == 'X' && board[1][1] == 'X' && board[2][0] == 'X') ||
     (board[0][2] == 'O' && board[1][1] == 'O' && board[2][0] == 'O')
   ) {
+    Array.from(diagonalCellsRight).forEach((cell) => {
+      cell.classList.add('winner');
+    });
     return true;
+  }
+};
+
+const checkForWin = () => {
+  //* Conditional statement  that will either announce a winner or change the marker.
+  if (horizontalWin() || verticalWin() || diagonalWin()) {
+    window.alert(`Player ${currentMarker} won!`);
+  } else {
+    changeMarker();
   }
 };
